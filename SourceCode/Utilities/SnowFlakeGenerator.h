12ccc8b1706cc4d2b86c7fa652f481ceaf714626
郑相关 <codingdogzxg@gmail.com>
2024-07-02 22:57:13 +0800

    feat: change epoch to 2022-9-30

--- SourceCode/Utilities/SnowFlakeGenerator.h
+++ SourceCode/Utilities/SnowFlakeGenerator.h
@@ -20,7 +20,7 @@ class SnowflakeGenerator : public ISnowflakeGenerator
     Uint64 m_sequence;
     Uint64 m_lastTimestamp;
 
-    static const Uint64 m_epoch         = 1609459200000ULL; // 2021-01-01 00:00:00 UTC
+    static const Uint64 m_epoch         = 1664496000000ULL; // 2022-09-30 00:00:00 UTC when Revelation was released by G.E.M.
     static const Uint64 m_machineIdBits = 10;
     static const Uint64 m_sequenceBits  = 12;
     static const Uint64 m_maxMachineId  = (1L << m_machineIdBits) - 1;
