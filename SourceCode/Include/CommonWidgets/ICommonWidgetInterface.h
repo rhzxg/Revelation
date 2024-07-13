a75000abeb6de081ed368356b00ccbc20481620f
郑相关 <codingdogzxg@gmail.com>
2024-07-14 01:08:09 +0800

    remove: remove useless header file

--- SourceCode/Include/CommonWidgets/ICommonWidgetInterface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IExtensionInterface.h"
-#include <QWidget>
-
-class ICommonWidgetInterface : public IExtensionInterface
-{
-  public:
-    virtual QWidget* CreateRevelationCalendarWidget() = 0;
-};
